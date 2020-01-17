d0e7c01ae23452e643f24307bd630b190fd3ec91
Alexis <umbrossa@gmail.com>
2020-01-17 11:40:39 +0100

    Adding package: react-native-paper, eslint, apollo, react-navigation

---
+++ App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Provider as PaperProvider, Button } from 'react-native-paper';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <View style={styles.container}>
+        <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+          Press me
+        </Button>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </PaperProvider>
+  );
+}
